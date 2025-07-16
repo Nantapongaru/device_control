aee857d0ec5101ee0862b8a663695354bcd6ca54
Nantapongaru <nantapong289@gmail.com>
2025-07-16 19:33:46 +0700

    อัปเดตไฟล์ทั้งหมดหลังจากกู้คืน work

---
+++ work/week-2.ino
@@ -0,0 +1,23 @@
+#define LED_BUTLTIN 2
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+ 
+  pinMode(LED_BUTLTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+
+  digitalWrite(LED_BUTLTIN, 1);
+  Serial.print("สวัสดี นันทพงศ์ อรุณเดช\n");
+  Serial.print("light ...");
+  delay(1000);
+
+  digitalWrite(LED_BUILTIN, 0);
+  Serial.print("สวัสดี นันทพงศ์ อรุณเดช\n");
+  Serial.print("not light ...");
+  delay(1000);
+}
---
+++ work/week-3.ino
@@ -0,0 +1,33 @@
+//EX2
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("นาย นันทพงศ์ อรุณเดช");
+  Serial.println("------ไฟสัญญาณจราจร-------");
+  
+}
+
+void loop() {
+  for (int i = 1; i <= 4; i++) {
+    Road(i);
+  }
+}
+
+
+void Road(int greenRoad) {
+  Serial.println("---------เริ่ม-----------");
+
+  for (int road = 1; road <= 4; road++) {
+    Serial.print(road);
+    Serial.print(" ");
+
+    if (road == greenRoad) {
+      Serial.println("is Green");
+    } else {
+      Serial.println("is Red");
+    }
+  }
+
+  Serial.println("--------------------");
+  delay(3000);  /
+}

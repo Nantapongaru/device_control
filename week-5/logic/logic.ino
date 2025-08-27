dea8ecc6ececec926852d211f4bb63fa40f98254
Nantapongaru <nantapong289@gmail.com>
2025-08-27 14:21:53 +0700

    week-9

--- week-5/logic/logic.ino
+++ week-5/logic/logic.ino
@@ -1,32 +1,29 @@
 void setup() {
- Serial.begin(9600);
- pinMode(2, OUTPUT);
-
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
   //เช็ค com port
-  if(Serial.available()){
+  if (Serial.available()) {
     String command = Serial.readStringUntil('\n');
     command.trim();
 
-    if(command == "ON"){
-      digitalWrite(2,1);
+    if (command == "ON") {
+      digitalWrite(2, 1);
 
-    }
-    else if (command == "OFF") {
+    } else if (command == "OFF") {
       digitalWrite(2, 0);
-   
-    }
-    else if ( command == "BLINK") {
+
+    } else if (command == "BLINK") {
       blinkLED();
     }
   }
 }
 
 
-void blinkLED(){
-  for (int i =0; i< 5; i++){
+void blinkLED() {
+  for (int i = 0; i < 5; i++) {
     digitalWrite(2, 1);
     delay(400);
     digitalWrite(2, 0);
